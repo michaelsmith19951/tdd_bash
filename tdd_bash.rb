6eca362806e13818dbe835794094438ca921d4b0
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-25 14:35:09 -0400

    Second test that passed

--- tdd_bash.rb
+++ tdd_bash.rb
@@ -1,5 +1,13 @@
 # First function with two tickets
 def number_comparison(myticket, winning_ticket)
-	1
+	myticket = myticket.split('')
+	winning_ticket = winning_ticket.split('')
+	matching_numbers = 0
+	winning_ticket.each_with_index do |num, index|
+		if num == myticket[index]
+			matching_numbers += 1
+		end
+	end
+	matching_numbers
 end
 
--- test_tdd_bash.rb
+++ test_tdd_bash.rb
@@ -15,4 +15,8 @@ class TestTddBash < Minitest::Test
 	def test_that_1_match_returns_1
 	assert_equal(1, number_comparison("1111", "1222"))
 	end
+
+	def test_that_2_match_returns_2
+	assert_equal(2, number_comparison("1111", "1122"))
+	end
 end
