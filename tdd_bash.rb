36ca3f6dfb3aa3f67385d2323f9f724ef0c354d2
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-25 16:04:28 -0400

    4th test that passed

--- tdd_bash.rb
+++ tdd_bash.rb
@@ -1,4 +1,4 @@
-# First function with two tickets
+# Function with two tickets
 def number_comparison(myticket, winningticket)
 	my_ticket = myticket.split('')
 	puts "My ticket is #{myticket}"
@@ -6,6 +6,7 @@ def number_comparison(myticket, winningticket)
 	puts "The winning ticket is #{winning_ticket}"
 	matching_numbers = 0
 	winning_ticket.each_with_index do |num, index|
+	puts "The number is #{num} and the index is #{index}."
 		if num == myticket[index]
 			matching_numbers += 1
 		end
